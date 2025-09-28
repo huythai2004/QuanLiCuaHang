3009d33ce954aedb3de021955693e3cb2d62a201
huythai200 <thaiphamhuy3@gmail.com>
2025-09-28 09:56:42 +0700

    update git.ignore rules

--- backend/BackEnd/src/main/java/com/example/backend/Repository/ProductsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.Repository;
-
-import com.example.backend.Entity.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductsRepository extends JpaRepository<Products,Integer> {
-}
