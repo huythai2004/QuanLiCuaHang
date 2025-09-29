cdf528a050bd35dafdaec5e6376074ca4f0c1df5
huythai200 <thaiphamhuy3@gmail.com>
2025-09-29 18:03:23 +0700

    Update CRUD Users

--- backend/BackEnd/src/main/java/com/example/backend/Repository/UserRepository.java
+++ backend/BackEnd/src/main/java/com/example/backend/Repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.example.backend.Repository;
 import com.example.backend.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
